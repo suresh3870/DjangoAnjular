d1138c00197dcc17862a1d7932f8c349550fcba8
suresh3870 <sksureshkumar88@gmail.com>
2018-12-13 17:42:02 +0530

    First commit

--- jango/scrumboard/serializer.py
+++ jango/scrumboard/serializer.py
@@ -2,11 +2,11 @@ from rest_framework import serializers
 
 from .models import List, Card
 
-class ListSerializer (serializers.ModelSerializer)
+class ListSerializer (serializers.ModelSerializer):
     class Meta:
         model = List
 
-class CardSerializer(serializers.ModelSerializer)
+class CardSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Card
