c3d91d40794b5860955d7fbbdbf9f08b1fb0ebf4
suresh3870 <sksureshkumar88@gmail.com>
2018-12-13 21:35:42 +0530

    Add list/Card API

--- jango/scrumboard/api.py
+++ jango/scrumboard/api.py
@@ -4,6 +4,7 @@ from rest_framework.generics import ListAPIView
 from .serializer import ListSerializer, CardSerializer
 from .models import List, Card
 
+
 class ListApi(ListAPIView):
     queryset = List.object.all()
     serilize_class = ListSerializer
