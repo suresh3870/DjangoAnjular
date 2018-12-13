228ff946b7dd09d4885e584d7667b29f92f20c9f
suresh3870 <sksureshkumar88@gmail.com>
2018-12-13 22:15:58 +0530

    Fix on serialize class name

--- jango/scrumboard/api.py
+++ jango/scrumboard/api.py
@@ -7,9 +7,9 @@ from .models import List, Card
 
 class ListApi(ListAPIView):
     queryset = List.objects.all()
-    serilizer_class = ListSerializer
+    serializer_class = ListSerializer
 
 
 class CardApi(ListAPIView):
         queryset = List.objects.all()
-        serilizer_class = CardSerializer
+        serializer_class = CardSerializer
