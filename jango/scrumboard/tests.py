1dde6ffdf8a14e9b31a2e1564de437b636f62ca3
suresh3870 <sksureshkumar88@gmail.com>
2018-12-14 12:39:23 +0530

    Adding Testclass to test Admin URL

--- jango/scrumboard/tests.py
+++ jango/scrumboard/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import RequestsClient
+
+
+class ApiTest(TestCase):
+    client = RequestsClient()
+    response = client.get('http://127.0.0.1:8000/admin/')
+    assert response.status_code == 200
