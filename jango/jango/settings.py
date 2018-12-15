bf748fb4ad5f7387c1bc8ec7e15b4967386a239b
suresh3870 <sksureshkumar88@gmail.com>
2018-12-15 22:50:28 +0530

    Docker- 1st commit

--- jango/jango/settings.py
+++ jango/jango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_nsch(=94cd$74ryt)h5@)$(^7o^z$*lg9y0na7giu5l$9u677'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.16.167.247']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
