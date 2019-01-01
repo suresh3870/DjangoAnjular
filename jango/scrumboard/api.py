d16ddf7e7eed52358b56aad442a700f94480aede
suresh3870 <sksureshkumar88@gmail.com>
2019-01-01 19:29:21 +0530

    adding auth

--- jango/scrumboard/api.py
+++ jango/scrumboard/api.py
@@ -2,17 +2,19 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializer import ListSerializer, CardSerializer
 from .models import List, Card
-
+from rest_framework import permissions
 
 
 class ListApi(ModelViewSet):
     queryset = List.objects.all()
     serializer_class = ListSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 
 class CardApi(ModelViewSet):
         queryset = Card.objects.all()
         serializer_class = CardSerializer
+        permission_classes = (permissions.IsAuthenticated,)
 
 
